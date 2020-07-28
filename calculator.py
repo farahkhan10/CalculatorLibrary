b771f45253a2e9b2b1a181921a73073eb3ec563f
farah <farahsultanafs79@gmail.com>
2020-07-28 17:56:32 -0400

    Add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+	return first_term + second_term
+
+def subtract(first_term, second_term):
+	return first_term - second_term
+
